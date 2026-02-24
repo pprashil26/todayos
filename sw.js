f24c5f7d5805d563c97bdc17d003895ca85aafc3
pprashil26 <prashildigital@gmail.com>
2026-02-24 14:45:05 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const cacheName='todayos-cache-v1';
+const assetsToCache=[
+  '/wrapper.html',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event=>{
+  event.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assetsToCache)));
+});
+
+self.addEventListener('fetch', event=>{
+  event.respondWith(caches.match(event.request).then(res=>res || fetch(event.request)));
+});
